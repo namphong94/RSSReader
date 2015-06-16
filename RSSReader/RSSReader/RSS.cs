7612cd26322c7d614854ca9177d1f6cf3cb256f8
tranuydu <tranuydu@gmail.com>
2015-06-16 14:07:58 +0700

    Commit the RSSReader code

---
+++ RSSReader/RSSReader/RSS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RSSReader
+{
+	public class RSS
+	{
+		public string Title { get; set; }
+		public string Link { get; set; }
+		public string Description { get; set; }
+	}
+}
+
