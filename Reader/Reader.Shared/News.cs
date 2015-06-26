af899bd55641c772edbc3545c37399ea295829d4
phongpham <phongpham@kms-technology.com>
2015-06-26 14:41:23 +0700

    update divide project

---
+++ Reader/Reader.Shared/News.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reader {
+	public class News {
+	
+		public News ()
+		{
+		}
+
+		public static List<News> CategoryList()
+		{
+			var newDataList = new List<News>();
+			newDataList.Add (new News ("Thoi su"));
+			newDataList.Add (new News ("Kinh doanh"));
+			newDataList.Add (new News ("Du lich"));
+			newDataList.Add (new News ("The thao"));
+
+			return newDataList;
+		}
+
+		public News (string newCategory = "Temporary Id")
+		{
+			Category = newCategory;
+		}
+
+		public string Category { get; set; }
+	}
+}
+
